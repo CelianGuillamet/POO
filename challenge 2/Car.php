bded7d0c7be14f7ced87edc68964c023ec69264d
CelianGuillamet <celian.guillamet@gmail.com>
2023-11-22 11:53:33 +0100

    challenge 2 fini

--- challenge 2/Car.php
+++ challenge 2/Car.php
@@ -3,6 +3,10 @@
 require_once 'Vehicle.php';
 class Car extends Vehicle
 {
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
     protected string $energy;
 
     protected int $energyLevel;
@@ -10,7 +14,7 @@ class Car extends Vehicle
     public function __construct(string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats);
-        $this->energy = $energy;
+        $this->setEnergy($energy);
     }
 
     public function getEnergy(): string
@@ -18,9 +22,12 @@ class Car extends Vehicle
         return $this->energy;
     }
 
-    public function setEnergy(string $energy): void
+    public function setEnergy(string $energy): Car
     {
-        $this->energy = $energy;
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
     }
 
     public function getEnergyLevel(): int
--- challenge 2/index.php
+++ challenge 2/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'Bicycle.php';
 require_once 'Car.php';
+require_once 'Truck.php';
 
 
 $bicycle = new Bicycle('blue', 1);
@@ -9,5 +10,16 @@ var_dump($bicycle);
 
 echo '<br><br>';
 $car = new Car('green', 4, 'electric');
+$car->setNbWheels(4);
 echo $car->forward();
 var_dump($car);
+
+
+echo '<br><br>';
+$truck1 = new Truck('red', 3, 'fuel', 100);
+$truck1->setNbWheels(8);
+echo $truck1->load(10);
+echo $truck1->load(90);
+echo $truck1->forward();
+echo $truck1->brake();
+var_dump($truck1);
